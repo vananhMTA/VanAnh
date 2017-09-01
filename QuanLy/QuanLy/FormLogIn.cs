524f84296787228f1cbf221f30200546e0deb5bb
vuongleMTA <31845237+vuongleMTA@users.noreply.github.com>
2017-09-02 00:41:28 +0700

    Login
    ahihi

--- QuanLy/QuanLy/FormLogIn.cs
+++ QuanLy/QuanLy/FormLogIn.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using InventoryManage.Models.Entity;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,13 +9,43 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace QuanLy
+namespace InventoryManage
 {
+    
     public partial class FormLogIn : Form
     {
+        
         public FormLogIn()
         {
             InitializeComponent();
+        }
+        MyContext db = new MyContext();
+        private void BtLog_Click(object sender, EventArgs e)
+        {
+            if(TxtLogName.Text == "" || TxtLogPass.Text == "")
+            {
+                MessageBox.Show("Enter UserName and Password");
+                return;
+            }
+            else
+            {
+                var result = db.Users.Where(p => p.UserName == TxtLogName.Text && p.Pass == TxtLogPass.Text).SingleOrDefault();
+                if(result == null)
+                {
+                    MessageBox.Show("Your Account doesn't exist");
+                    TxtLogName.Clear();
+                    TxtLogPass.Clear();
+                }
+                else
+                {
+                    this.Hide();
+                    MessageBox.Show("Log in successfully");
+                    Medium.IdSto = result.ID;
+                    Medium.Active = true;
+                    FormHome f = new FormHome();
+                    f.Show();
+                }
+            }
         }
     }
 }
